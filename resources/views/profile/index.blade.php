a6f1cf5092168aaf82639d6e53ad934678ce706c
Mauryasuraj <surajmaurya>
2019-06-25 14:09:05 +0530

    Profile index chnages

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -22,8 +22,8 @@
                                             <div class="col-md-12">
                                                 <h5>Personal Details</h5>
                                                 <h3 class="h3 my-2">{{ $user->name }}</h3>
-                                                <p class="my-1">Phone No : <strong>{{ $user->profile->description }}</strong></p>
-                                                <p class="my-1">Email Id: <strong>{{ $user->email }}</strong></p>
+                                                <p class="my-1 font-weight-bold">Phone No : <strong>{{ $user->profile->phone }}</strong></p>
+                                                <p class="my-1 font-weight-bold">Email Id: <strong>{{ $user->email }}</strong></p>
                                                 <div class="d-flex">
                                                     <img src="https://image.flaticon.com/icons/svg/179/179319.svg"  alt="" class="mr-2 " style="height: 40px; width: 40px;">
                                                     <img src="https://image.flaticon.com/icons/svg/145/145812.svg"  alt="" class="mr-2 " style="height: 40px; width: 40px;">
@@ -33,8 +33,13 @@
                                             </div>
                                             <div class="col-md-12 my-2">
                                                 <h2>Description</h2>
-                                                <p class="text-dark text-justify p-2">
+                                                <p class="text-dark text-justify p-2 mx-2">
                                                    {{ $user->profile->description }}
+                                                </p>
+
+                                                <h2>Address</h2>
+                                                <p class="text-dark text-justify p-2 mx-2">
+                                                    {{ $user->profile->address }}
                                                 </p>
                                                 <h2>Membership Details</h2>
                                                 <p>
